e5d88f051a37404961319a1e0d5b77fca85b71d9
Aditya <adityaubale63@gmail.com>
2023-04-08 20:55:00 +0530

    backend completed

--- backend/data.js
+++ backend/data.js
@@ -28,4 +28,32 @@ export const products =  [
         price: 15000,
         description:"Desktop description"
     },
+    {
+        productId:ulid(), 
+        productName: 'Keyboard', 
+        modelYear: "2020", 
+        price: 1000,
+        description:"keyboard description"
+    },
+    {
+        productId:ulid(), 
+        productName: 'Laptop', 
+        modelYear: "2021", 
+        price: 25000,
+        description:"Laptop description"
+    },
+    {
+        productId:ulid(), 
+        productName: 'Inverter', 
+        modelYear: "2022", 
+        price: 10000,
+        description:"Inverter description"
+    },
+    {
+        productId:ulid(), 
+        productName: 'Desktop', 
+        modelYear: "2018", 
+        price: 15000,
+        description:"Desktop description"
+    },
 ]
